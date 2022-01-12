7b511436597b3687bd56cbd21489a67a1a1e8e45
Shahzaib Ekram <64416394+shahzaibekram@users.noreply.github.com>
2022-01-13 00:22:36 +0500

    lib

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Library Management System |<a href="https://www.linkedin.com/in/manish-sahani/" target="_blank"  > Designed by : Manish Sahani</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
